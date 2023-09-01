be7face7dc6a2e4f3b97d37e181d52648151c9b2
faryyym <mohamed.fareem@gmail.com>
2023-09-01 20:34:38 +0500

    Add: Timline Comp. to iterate over each object

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+import Timeline from './views/Timeline.vue'
+</script>
 
 <template>
   <h1>Timeline</h1>
+  <Timeline />
 </template>
 
 <style scoped></style>
---
+++ src/views/Timeline.vue
@@ -0,0 +1,54 @@
+<!-- LOGIC -->
+<script>
+import historyData from '../../db.json'
+export default {
+  name: 'Timeline',
+  data() {
+    return {
+      historyData,
+    }
+  },
+}
+</script>
+
+<!-- VIEW -->
+<template>
+  <div
+    v-for="events in historyData"
+    class="timeline-card-container"
+  >
+    <div class="timeline-card">
+      <img :src="events?.imageUrl" alt="" />
+      <h3>{{ events.title }}</h3>
+      <p>{{ events.description }}</p>
+      <div class="period">
+        Period
+        <div class="years">
+          <p>
+            {{ events.start_year }}
+            <span v-if="events.end_year">-</span>
+            {{ events?.end_year }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<!-- STYLES -->
+<style scoped lang="scss">
+.timeline-card-container {
+  .timeline-card {
+    img {
+      width: 250px;
+      height: 250px;
+      object-fit: cover;
+      object-position: center;
+
+      &[src=''] {
+        display: none;
+      }
+    }
+  }
+}
+</style>
