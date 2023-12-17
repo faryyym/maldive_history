6068c1e051914ebe867e2ad6e41dc10d894cf113
faryyym <mohamed.fareem@gmail.com>
2023-12-18 00:21:38 +0500

    More Clean Up

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import './style.scss'
 import App from './App.vue'
-import { MotionPlugin } from '@vueuse/motion'
 
-createApp(App).use(MotionPlugin).mount('#app')
+createApp(App).mount('#app')
